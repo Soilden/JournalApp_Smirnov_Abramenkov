adfb6abbc9c75a651d021502b2fd80decdd5860a
Max <65918596+Busted89@users.noreply.github.com>
2023-03-29 08:32:33 +0300

    02:09

--- JournalApp/ViewModels/MainWindowViewModel.cs
+++ JournalApp/ViewModels/MainWindowViewModel.cs
@@ -7,25 +7,38 @@ using JournalApp.ViewModels.Base;
 
 namespace JournalApp.ViewModels
 {
-    internal class MainWindowViewModel : ViewModel
+    internal class MainWindowViewModel : Base.ViewModel
     {
         #region Заголовок окна
         private string _Title = "Анализ статистики CV19";
 
-        /// <summary>Заголовок окна</summary>
-        private string Title 
+        private string Title
         {
             get => _Title;
-            //set
-            //{
-            //    //if (Equals(_Title, value)) return;
-            //   //_Title = value;
-            //   //OnPropertyChanged();
-            
-            //   Set(ref _Title, value);
-            //}
-            set => Set(ref _Title, value);
+            /*set
+            {
+                *//*if (Equals(_Title, value)) return;
+                _Title = value;
+                OnPropertyChanged();
+
+                Set(ref _Title, value);
+            }*/
+                set => Set(ref _Title, value);
+            }
+        #endregion
+
+        #region Status : string - Статус программы
+
+        /// <summary>Статус программы</summary>
+        private string _Status = "Готов!";
+
+        /// <summary>Статус программы</summary>
+        public string Status
+        {
+            get => _Status;
+            set => Set(ref _Status, value);
         }
+
         #endregion
     }
 }
