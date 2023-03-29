e779ee5b8e4bf4e55b3db93c9c294a04494a67ff
Max <65918596+Busted89@users.noreply.github.com>
2023-03-29 11:38:39 +0300

    3.10

--- JournalApp/ViewModels/MainWindowViewModel.cs
+++ JournalApp/ViewModels/MainWindowViewModel.cs
@@ -4,10 +4,14 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using JournalApp.ViewModels.Base;
+using JournalApp.ViewModels;
+using JournalApp.Infrastructure.Commands;
+using System.Windows.Input;
+using System.Windows;
 
 namespace JournalApp.ViewModels
 {
-    internal class MainWindowViewModel : Base.ViewModel
+    internal class MainWindowViewModel : ViewModel
     {
         #region Заголовок окна
         private string _Title = "Анализ статистики CV19";
@@ -40,5 +44,28 @@ namespace JournalApp.ViewModels
         }
 
         #endregion
+
+        #region Команды
+
+        #region CloseApplicationCommand
+        public ICommand CloseApplicationCommand { get; }
+
+        private bool CanCloseApplicationCommandExecuted(object p) => true;
+
+        private void OnCloseApplicationCommandExecuted(object p)
+        {
+            Application.Current.Shutdown();
+        }
+        #endregion
+
+        #endregion
+        public MainWindowViewModel()
+        {
+            #region Команды
+
+            CloseApplicationCommand = new LambdaCommand(OnCloseApplicationCommandExecuted, CanCloseApplicationCommandExecuted);
+
+            #endregion
+        }
     }
 }
